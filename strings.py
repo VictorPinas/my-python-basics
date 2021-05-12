513c9324e63eb88b4a2cc33421bb2a256cc47733
Victor Pinas Arnault <49802514+VictorPinas@users.noreply.github.com>
2021-05-12 16:59:59 -0300

    Add files via upload
    My first tries.

---
+++ strings.py
@@ -0,0 +1,31 @@
+# print a welcome message
+
+# print('Hello World')
+# message = 'Hello World'
+# greeting = 'Hello'
+# name = 'Victor'
+# message = greeting + name
+# message =  greeting + ', ' + name + '. Welcome!'
+# message = '{}, {}. Welcome!'.format(greeting, name)
+# message = f'{greeting}, {name}. Welcome!'
+# new_message = message.replace('World', 'Universe')
+# message = "Bobby's World"
+# message = 'Bobby\'s World'
+# message = """Bobby's World was a good cartoon in the 1990's"""
+# print(message)
+# print(dir(name))
+# print(help(str))
+# print(help(str.lower))
+# print(len(message))
+# print(message[0])
+# print(message[10])
+# print(message[0:5])
+# print(message[:5])
+# print(message[6:5])
+# print(message[6:])
+# print(message.lower())
+# print(message.upper())
+# print(message.count('Hello'))
+# print(message.count('l'))
+# print(message.find('World'))
+# print(message.find('Universer'))
