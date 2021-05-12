fb97b6b4bd07c747fd59468fe2f0a67dcb7af7a4
Victor Pinas Arnault <49802514+VictorPinas@users.noreply.github.com>
2021-05-12 17:40:53 -0300

    Add files via upload
    integers and floats

---
+++ numbers.py
@@ -0,0 +1,40 @@
+# integers and floats
+
+# num = 3
+# print(type(num))
+
+# print(3 + 2)
+# print(3 - 2)
+# print(3 * 2)
+# print(3/2)
+# print(3//2)
+# print(3 ** 2)
+# print(3 % 2)
+
+# print(3 * 2 + 1)
+# print(3 * (2 +1))
+
+# num = 1
+# num = num + 1
+# num += 1
+# num *= 10
+#print(num)
+
+# print(abs(-3))
+# print(round(3.75))
+# print(round(3.75, 1))
+
+# num_1 = 3
+# num_2 = 2
+# print(num_1 == num_2)
+# print(num_1 != num_2)
+# print(num_1 < num_2)
+# print(num_1 > num_2)
+# print(num_1 >= num_2)
+# print(num_1 <= num_2)
+
+# num_1 = '100'
+# num_2 = '200'
+# num_1 = int(num_1)
+# num_2 = int(num_2)
+# print(num_1 + num_2)
