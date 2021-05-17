83e6abd6c2cf7ce286fd2f18116132f6809ce31a
Victor Pinas Arnault <49802514+VictorPinas@users.noreply.github.com>
2021-05-17 13:40:47 -0300

    Add files via upload
    dictionaries

---
+++ dicts.py
@@ -0,0 +1,32 @@
+# dictionaries allow us to work with key, value pairs
+# if you're coming from another programming language, you heard of it as hash maps or
+# associative arrays
+
+# the key, value pairs are two linked values where the key is a unique identifer
+# where we can find our data and the value is that data
+
+student = {'name': 'John', 'age': 25, 'courses': ['Math','CompSci']}
+# student['phone'] = '(11)991111111'
+# student['name'] = 'Jane'
+# student.update({'name': 'Jane', 'age': 35, 'courses':['Arts', 'Acting'], 'phone': '(22)992222222'})
+# del student['age']
+# age = student.pop('age')
+# student = {1: 'John', 'age': 25, 'courses': ['Math','CompSci']}
+# print(student)
+# print(age)
+# print(student['name'])
+# print(student[1])
+# print(student.get('name'))
+# print(student.get('phone'))
+# print(student.get('phone', 'Not Found'))
+
+# print(len(student))
+# print(student.keys())
+# print(student.values())
+# print(student.items())
+
+# for key in student:
+    # print(key)
+
+# for key, value in student.items():
+    # print(key, value)
